b7fee57cb74ed429bd1de84abe2bbd596bd26d09
Kale-Ko <github@kaleko.dev>
2021-11-02 20:18:57 -0400

    Update

--- server/Server.js
+++ server/Server.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const express = require("express")
+const http = require("http")
 const WebSocket = require("websocket").server
 const Packet = require("../common/Packet.js")
 
@@ -14,30 +14,28 @@ module.exports = class Server {
         if (port == undefined || port == null) throw new Error('Missing paramiter "port"')
         if (origin == undefined || origin == null) throw new Error('Missing paramiter "origin"')
 
-        this.httpServer = express()
+        this.httpServer = http.createServer((req, res) => {
+            if (req.url == "/") {
+                res.statusCode = 200
+                res.statusMessage = "Ok"
+                res.end(fs.readFileSync("./server/index.html").toString().replace(/{origin}/g, this.origin.replace("http", "ws")))
+            } else if (req.url == "/Packet.js") {
+                res.statusCode = 200
+                res.statusMessage = "Ok"
+                res.end(fs.readFileSync("./common/Packet.js").toString().replace(/{origin}/g, this.origin.replace("http", "ws")))
+            } else {
+                res.statusCode = 404
+                res.statusMessage = "Not Found"
+                res.end("404 Not Found")
+            }
+        })
         this.server = new WebSocket({ httpServer: this.httpServer, maxReceivedFrameSize: 100000000, maxReceivedMessageSize: 100000000, fragmentationThreshold: 5000000, keepaliveInterval: 5000, parseCookies: false })
         this.origin = origin
         this.port = port
     }
 
     start() {
-        this.httpServer.get("/", (req, res) => {
-            res.statusCode = 200
-            res.statusMessage = "Ok"
-            res.end(fs.readFileSync("./server/index.html").toString().replace(/{origin}/g, this.origin.replace("http", "ws")))
-        })
-
-        this.httpServer.get("/Packet.js", (req, res) => {
-            res.statusCode = 200
-            res.statusMessage = "Ok"
-            res.end(fs.readFileSync("./common/Packet.js").toString().replace(/{origin}/g, this.origin.replace("http", "ws")))
-        })
-
-        this.httpServer.get("*", (req, res) => {
-            res.statusCode = 404
-            res.statusMessage = "Not Found"
-            res.end("404 Not Found")
-        })
+        this.httpServer.listen(this.port)
 
         this.server.on("request", (req) => {
             if (req.origin == this.origin || req.origin == undefined) req.accept()
@@ -51,7 +49,5 @@ module.exports = class Server {
                 this.server.broadcast(message.encode())
             })
         })
-
-        this.httpServer.listen(this.port)
     }
 }

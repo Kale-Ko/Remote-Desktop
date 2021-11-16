425167f21f414969d007cabb26dad9b112547745
Kale-Ko <github@kaleko.dev>
2021-11-16 12:28:54 -0500

    Update

--- src/Server.js
+++ src/Server.js
@@ -56,17 +56,37 @@ module.exports = class Server {
                                 res.statusMessage = "Ok"
                                 res.end(new Packet("displays", displaysdata).encode())
                             })
-                        } else if (req.url == "control") {
-                            // if (message.type == "mousemove") {
-                            //     robot.moveMouseSmooth(message.data.x, message.data.y, 1)
-                            // } else if (message.type == "mouseclick") {
-                            //     robot.mouseClick(message.data.button, false)
-                            // } else if (message.type == "mousescroll") {
-                            //     robot.scrollMouse(message.data.x, message.data.y)
-                            // } else if (message.type == "keypress") {
-                            //     if (message.data.type) robot.typeString(message.data.string)
-                            //     else robot.keyTap(message.data.key, message.data.modifiers)
-                            // }
+                        } else if (req.url == "/display") {
+                            var message = Packet.decode(req.headers.packet)
+
+                            screenshot({ screen: message.data.id }).then(async display => {
+                                var size = imageSize(display)
+
+                                var image = await sharp(display, { sequentialRead: true })
+                                    .webp({ quality: 50, alphaQuality: 0, reductionEffort: 6 })
+                                    .composite([{ input: mouse, top: Math.floor(robot.getMousePos().y / 2), left: Math.floor(robot.getMousePos().x / 2) }])
+                                    .resize(Math.floor(size.width / 2), Math.floor(size.height / 2))
+                                    .sharpen()
+                                    .normalize()
+                                    .toBuffer()
+
+                                res.statusCode = 200
+                                res.statusMessage = "Ok"
+                                res.end(new Packet("display", { id: message.data.id, size, image }).encode())
+                            })
+                        } else if (req.url == "/control") {
+                            var message = Packet.decode(req.headers.packet)
+
+                            if (message.type == "mousemove") {
+                                robot.moveMouseSmooth(message.data.x, message.data.y, 1)
+                            } else if (message.type == "mouseclick") {
+                                robot.mouseClick(message.data.button, false)
+                            } else if (message.type == "mousescroll") {
+                                robot.scrollMouse(message.data.x, message.data.y)
+                            } else if (message.type == "keypress") {
+                                if (message.data.type) robot.typeString(message.data.string)
+                                else robot.keyTap(message.data.key, message.data.modifiers)
+                            }
                         } else {
                             res.statusCode = 404
                             res.statusMessage = "Not Found"

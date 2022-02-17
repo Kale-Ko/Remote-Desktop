a323525de0403ac7f06f573add8421e7b5a201f2
Kale-Ko <github@kaleko.dev>
2022-02-16 23:28:42 -0500

    Update

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/**
-    @license
-    MIT License
-    Copyright (c) 2021 Kale Ko
-    See https://kaleko.ga/license.txt
-*/
-
 const Config = require("./src/Config.js")
 const Server = require("./src/Server.js")
 
